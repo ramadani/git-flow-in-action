a90d0720ac1712fd0e188001b2333a30d5ebe8d4
Ramadani <email.ramadani@gmail.com>
2018-07-19 16:11:09 +0700

    register feature

--- main.go
+++ main.go
@@ -12,9 +12,14 @@ func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Login Route\n")
 }
 
+func Register(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Register Route\n")
+}
+
 func main() {
 	router := httprouter.New()
 	router.POST("/login", Login)
+	router.POST("/register", Register)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
